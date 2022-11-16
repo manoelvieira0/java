af9807fc1ed4a8ff5f7049748d8fd5d0fe35d735
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-16 07:07:38 -0300

    Estrutura repetição while

--- java_basics/src/java_basics/Main.java
+++ java_basics/src/java_basics/Main.java
@@ -8,40 +8,18 @@ public class Main {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-
-		Scanner sc = new Scanner(System.in);
-
-		int x = sc.nextInt();
-		String dia;
 		
-		switch (x) {
-		case 1:
-			dia = "domingo";
-			break;
-		case 2:
-			dia = "segunda";
-			break;
-		case 3:
-			dia = "terca";
-			break;
-		case 4:
-			dia = "quarta";
-			break;
-		case 5:
-			dia = "quinta";
-			break;
-		case 6:
-			dia = "sexta";
-			break;
-		case 7:
-			dia = "sabado";
-			break;
-		default:
-			dia = "valor invalido";
-			break;
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int soma = 0;
+		
+		while (x != 0) {
+			soma += x;
+			x = sc.nextInt();
 		}
 		
-		System.out.println("Dia da semana: " + dia);
+		System.out.println(soma);
 		
 		sc.close();
 
