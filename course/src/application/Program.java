c122ccef2383b52bbde6323681ed5304c36fb835
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-17 09:01:34 -0300

    Método Construtor

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -3,26 +3,43 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Employee;
-import utils.Calculator;
+import utils.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Informe o raio: ");
-		double raio = sc.nextDouble();
+		System.out.println("Enter product data: ");
 		
-		double c = Calculator.circuferencia(raio);
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		int quantity = sc.nextInt();
 		
-		double v = Calculator.volume(raio);
+		Product product = new Product(name, price, quantity);
 		
-		System.out.printf("Circuferência: %.2f%n", c);
-		System.out.printf("Volume: %.2f%n", v);
-		System.out.println("Pi: " + Calculator.PI);
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
 
 		sc.close();
 	}
