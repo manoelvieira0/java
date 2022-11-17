7643f8552c55845eb007a6094251805f47174bfe
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-17 06:27:06 -0300

    classe Calculator com membros de instância

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -4,36 +4,29 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Employee;
+import utils.Calculator;
 
 public class Program {
-	
-	public static final double PI = 3.14159;
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 
 		Scanner sc = new Scanner(System.in);
 		
+		Calculator calc = new Calculator();
+		
 		System.out.print("Informe o raio: ");
 		double raio = sc.nextDouble();
 		
-		double c = circuferencia(raio);
+		double c = calc.circuferencia(raio);
 		
-		double v = volume(raio);
+		double v = calc.volume(raio);
 		
 		System.out.printf("Circuferência: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.println("Pi: " + PI);
+		System.out.println("Pi: " + calc.PI);
 
 		sc.close();
-	}
-	
-	public static double circuferencia(double raio) {
-		return 2.0 * PI * raio;
-	}
-	
-	public static double volume(double raio) {
-		return 4 * PI * raio * raio * raio / 3.0;
 	}
 
 }
