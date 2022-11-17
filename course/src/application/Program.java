fb0bd2044b6707da555e7efec5d435399b5d1e5d
netovieira01_ <netinhovieira1801@gmail.com>
2022-11-17 07:00:47 -0300

    classe Calculator com método estático

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -13,18 +13,16 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		
-		Calculator calc = new Calculator();
-		
 		System.out.print("Informe o raio: ");
 		double raio = sc.nextDouble();
 		
-		double c = calc.circuferencia(raio);
+		double c = Calculator.circuferencia(raio);
 		
-		double v = calc.volume(raio);
+		double v = Calculator.volume(raio);
 		
 		System.out.printf("Circuferência: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.println("Pi: " + calc.PI);
+		System.out.println("Pi: " + Calculator.PI);
 
 		sc.close();
 	}
